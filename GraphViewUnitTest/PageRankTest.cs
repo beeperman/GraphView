796b6fae494b4f2707e0979c5aec139a99f65147
yanjga <yanjga@microsoft.com>
2017-04-01 10:46:34 +0800

    Add approximate PageRank test suite.

---
+++ GraphViewUnitTest/PageRankTest.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using GraphView;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphView;
+
+namespace GraphViewUnitTest
+{
+    [TestClass]
+    public class PageRankTest
+    {
+        [TestMethod]
+        public void LoadClassicGraphData()
+        {
+            GraphViewConnection connection = new GraphViewConnection("https://graphview.documents.azure.com:443/",
+              "MqQnw4xFu7zEiPSD+4lLKRBQEaQHZcKsjlHxXn2b96pE/XlJ8oePGhjnOofj1eLpUdsfYgEhzhejk2rjH/+EKA==",
+              "GroupMatch", "MarvelTest");
+            connection.ResetCollection();
+            GraphViewCommand graphCommand = new GraphViewCommand(connection);
+
+            graphCommand.g().AddV("person").Property("name", "marko").Property("age", 29).Next();
+            graphCommand.g().AddV("person").Property("name", "vadas").Property("age", 27).Next();
+            graphCommand.g().AddV("software").Property("name", "lop").Property("lang", "java").Next();
+            graphCommand.g().AddV("person").Property("name", "josh").Property("age", 32).Next();
+            graphCommand.g().AddV("software").Property("name", "ripple").Property("lang", "java").Next();
+            graphCommand.g().AddV("person").Property("name", "peter").Property("age", 35).Next();
+            graphCommand.g().V().Has("name", "marko").AddE("knows").Property("weight", 0.5d).To(graphCommand.g().V().Has("name", "vadas")).Next();
+            graphCommand.g().V().Has("name", "marko").AddE("knows").Property("weight", 1.0d).To(graphCommand.g().V().Has("name", "josh")).Next();
+            graphCommand.g().V().Has("name", "marko").AddE("created").Property("weight", 0.4d).To(graphCommand.g().V().Has("name", "lop")).Next();
+            graphCommand.g().V().Has("name", "josh").AddE("created").Property("weight", 1.0d).To(graphCommand.g().V().Has("name", "ripple")).Next();
+            graphCommand.g().V().Has("name", "josh").AddE("created").Property("weight", 0.4d).To(graphCommand.g().V().Has("name", "lop")).Next();
+            graphCommand.g().V().Has("name", "peter").AddE("created").Property("weight", 0.2d).To(graphCommand.g().V().Has("name", "lop")).Next();
+
+            graphCommand.Dispose();
+            connection.Dispose();
+        }
+        [TestMethod]
+        public void runPageRank()
+        {
+            GraphViewConnection connection = new GraphViewConnection("https://graphview.documents.azure.com:443/",
+            "MqQnw4xFu7zEiPSD+4lLKRBQEaQHZcKsjlHxXn2b96pE/XlJ8oePGhjnOofj1eLpUdsfYgEhzhejk2rjH/+EKA==",
+            "GroupMatch", "MarvelTest");
+            GraphViewCommand graph = new GraphViewCommand(connection);
+            graph.OutputFormat = OutputFormat.GraphSON;
+            var src1 = graph.g().V().HasLabel("person").Has("name", Predicate.eq("marko")).Values("id").Next()[0];
+            Dictionary<String, VertexMetrics> vertexCacheMetrics = new Dictionary<string, VertexMetrics>();
+            HashSet<String> nonConvergenceVertex = new HashSet<string>();
+            var vertexs = JsonConvert.DeserializeObject < JArray >(graph.g().V().Next().FirstOrDefault());
+            foreach(var v in vertexs)
+            {
+                var id = v["id"];
+                nonConvergenceVertex.Add(id.ToString());
+            }
+
+            var bound = 0.01;
+            var iterNum = 100;
+            while(iterNum > 0)
+            {
+                iterNum--;
+                if(nonConvergenceVertex.Count == 0)
+                {
+                    break;
+                }
+                // (1) only process the nonConverge vertex
+                var vs = JsonConvert.DeserializeObject<JArray>(graph.g().V(nonConvergenceVertex.ToArray()).Next().FirstOrDefault());
+                foreach(var v in vs)
+                {
+                    var vertexId = v["id"].ToString();
+                    var inEdges = v["inE"];
+                    var incW = 0.0;
+                    // (2) only process the nonConverge inEdge vertex
+                    if (inEdges != null)
+                    {
+                        foreach (var e in inEdges)
+                        {
+                            if (!vertexCacheMetrics.ContainsKey(vertexId))
+                            {
+                                vertexCacheMetrics[vertexId] = new VertexMetrics();
+                            }
+                            var tmpE = e.First.First;
+                            var inVId = tmpE["outV"].ToString();
+                            if (nonConvergenceVertex.Contains(inVId))
+                            {
+                                if (!vertexCacheMetrics.ContainsKey(inVId))
+                                {
+                                    vertexCacheMetrics[inVId] = new VertexMetrics();
+                                }
+                                var w = vertexCacheMetrics[inVId].convergeVertexWeightSum;
+                                var size = inEdges.First.First.Count(); // need to refactor for diff edge type
+                                incW += w / size;
+                                // find the last iter value
+                                if(!vertexCacheMetrics[vertexId].inVWeight.ContainsKey(inVId))
+                                {
+                                    vertexCacheMetrics[vertexId].inVWeight[inVId] = 0.0;
+                                }
+                                vertexCacheMetrics[vertexId].inVWeight[inVId] = incW;
+
+                                if (Math.Abs(incW - vertexCacheMetrics[vertexId].inVWeight[inVId]) < 0.01)
+                                {
+                                    // (3) inc the converge vertex weight
+                                    nonConvergenceVertex.Remove(vertexId);
+                                    vertexCacheMetrics[vertexId].convergeVertexWeightSum += incW;
+                                }
+                            }
+
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("The Program finished");
+        }
+    }
+
+    public class VertexMetrics
+    {
+        public Double convergeVertexWeightSum = 1.0;
+        public Dictionary<String, Double> inVWeight = new Dictionary<string, double>();
+    }
+}
